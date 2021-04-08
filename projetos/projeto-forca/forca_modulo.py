3228fe10b4f1e0c67d2af84742f591a736e3021e
MatheusJanibelli <janibellim@gmail.com>
2021-04-08 17:21:03 -0300

    Adiciona forca_modulo.py

---
+++ projetos/projeto-forca/forca_modulo.py
@@ -0,0 +1,12 @@
+def encaixar_letra(letra_esc, palavra_maquina, palavra_secreta):
+    """Coloca a letra escolhida pelo usuário na palavra secreta, 
+    no lugar e na quantidade de vezes que ela aparece na palavra escolhida pela máquina."""
+    palavra_lista = list(palavra_secreta)
+
+    for index, letra in enumerate(list(palavra_maquina)):
+        if letra == letra_esc:
+            palavra_lista[index] = letra_esc
+        
+    palavra_secreta = "".join(palavra_lista)
+
+    return palavra_secreta
