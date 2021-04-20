60552115d1dade976160dddcea820089aa904b3e
MatheusJanibelli <janibellim@gmail.com>
2021-04-20 08:24:17 -0300

    Muda variáveis e condicional

--- projetos/projeto-alarme/alarme.py
+++ projetos/projeto-alarme/alarme.py
@@ -7,9 +7,9 @@ data_marcada = input("Digite a data  (Formato: 'Dia/Mês/Ano'): ")
 hora_marcada = input("Digite a hora (Formato: 'Horas:Minutos', 24 horas): ")
 
 while True:
-    hora = datetime.datetime.now()
-    data = datetime.date.today()
+    hora_atual = datetime.datetime.now().strftime("%H:%M")
+    data_atual = datetime.date.today().strftime("%d/%m/%Y")
 
-    if data.strftime("%d/%m/%Y") == data_marcada and hora.strftime("%H:%M") == hora_marcada:
+    if data_atual == data_marcada and hora_atual == hora_marcada:
         winsound.Beep(2500, 10000)
         break
