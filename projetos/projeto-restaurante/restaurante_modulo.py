a3f08faa776efab684f7e4cedcc6139898643e37
MatheusJanibelli <janibellim@gmail.com>
2021-04-30 08:17:57 -0300

    Adiciona docstrings

--- projetos/projeto-restaurante/restaurante_modulo.py
+++ projetos/projeto-restaurante/restaurante_modulo.py
@@ -1,11 +1,32 @@
+"""Módulo para simular um restaurante"""
+
+
 class Restaurante:
+    """Classe que representa um restaurante.
+
+    Atributos:
+        nome (str): Nome do restaurante.
+        rendimento (float): Rendimento do restaurante. Default = 0.0
+        clientes (int): Número de clientes atendidos. Default = 0
+    """
 
     def __init__(self, nome):
+        """Inicializa as variáveis de instância."""
         self.nome = nome
-        self.rendimento = 0
+        self.rendimento = 0.0
         self.clientes = 0
 
-    def atender_cliente(self, preco_produto):
+    def atender_cliente(self, preco_produto: float) -> bool:
+        """Simula o atendimento a um único cliente.
+        
+        Args:
+            preco_produto: Preço da refeição do cliente.
+        
+        Retorna:
+            True, caso preco_produto seja positivo.
+            False, caso preco_produto seja negativo.
+        """
+        
         if preco_produto > 0:
             self.rendimento += preco_produto
             self.clientes += 1
